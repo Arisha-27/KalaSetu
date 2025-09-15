9606ac992be6bfbfeedaee9ff52799e029af6a79
Afsha Zareen <afshazareen117@gmail.com>
2025-09-15 16:05:11 +0530

    db connect

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\anaya\OneDrive\Documents\Documents\MarketPlace-Assistant\backend\venv
+set VIRTUAL_ENV=C:\Users\HP\MarketPlace-Assistant-3\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- src/pages/AIListingGenerator.tsx
+++ src/pages/AIListingGenerator.tsx
@@ -1,3 +1,13 @@
+// supabaseClient.js
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://obllonwzdjnokqeyvtsi.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ibGxvbnd6ZGpub2txZXl2dHNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY5ODQzMjIsImV4cCI6MjA3MjU2MDMyMn0.4hxTc9zEL-GBG2k1pDBUjtQIQHWgYRgoPfOSQ6uOAJc'  
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+
+
 import { useState } from "react"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
