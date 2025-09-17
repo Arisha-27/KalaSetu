49bb1e1920642b586a38ba00519cdab531e15adf
Arisha-27 <arisharizwan786@gmail.com>
2025-09-17 23:18:51 +0530

    fix: Resolve vite.config.ts conflict

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
@@ -9,32 +9,14 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-<<<<<<< HEAD
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-=======
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    // This plugin is only added when you are in development mode
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
->>>>>>> 93ec03dfc2a662c75957e56607bc43c05e1ce8f7
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
-<<<<<<< HEAD
-=======
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> 93ec03dfc2a662c75957e56607bc43c05e1ce8f7
